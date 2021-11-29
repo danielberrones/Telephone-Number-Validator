75dbec0de06504c9316a1d18fbea4541fd64b964
DBerrones <53327546+danielberrones@users.noreply.github.com>
2021-11-28 21:38:31 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+function telephoneCheck(str) {
+  let trueRe = [/\d{10}/ig,
+                /\d{9}/ig,
+                /^\d{3}-\d{3}-\d{4}$/ig,
+                /\d{1} \d{3} \d{3} \d{4}/ig,
+                /\(\d{3}\)\d{3}-\d{4}/ig,
+                /\d{1} \(\d{3}\) \d{3}-\d{4}/ig,
+                /1 \d{3}-\d{3}-\d{4}/ig];
+  let falseRe = [/\(\d{10}\)/ig, 
+                /(0|2|-1) \(\d{3}\) \d{3}-\d{4}/ig, 
+                /\d{11}/ig, /2\(\d{3}\)\d{3}-\d{4}/ig]
+     
+for (let i = 0; i < falseRe.length; i++) {
+  if (str.match(falseRe[i])) {
+      return false;
+  }
+}
+  
+for (let i = 0; i < trueRe.length; i++) {
+  if (str.match(trueRe[i])) {
+      return true;
+  }
+}
+  
+return false;
+}
+
+
+telephoneCheck("555-555-5555")
